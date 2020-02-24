6a60a2c3041d3c4bfedde491ee3ca837bbb754b8
vlukyane <vlukyane@yandex.ru>
2020-02-24 16:50:22 +0300

    init ssr proj

---
+++ frontend-ssr/.next/static/webpack/static/development/pages/index.js.c888df898ab28a2e4cff.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate("static/development/pages/index.js",[
+/* 0 */,
+/* 1 */
+false,
+/* 2 */
+/*!************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fvlukyane%2FDocuments%2Fpet_project%2Ffrontend-ssr%2Fpages%2Findex.js ***!
+  \************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fvlukyane%2FDocuments%2Fpet_project%2Ffrontend-ssr%2Fpages%2Findex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fvlukyane%2FDocuments%2Fpet_project%2Ffrontend-ssr%2Fpages%2Findex.js!./");
+
+
+/***/ })
+])
+//# sourceMappingURL=index.js.c888df898ab28a2e4cff.hot-update.js.map
---
+++ frontend-ssr/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            <p> Hello kek shrek </p>
+        </div>
+    )
+};
+
+export default Index;
